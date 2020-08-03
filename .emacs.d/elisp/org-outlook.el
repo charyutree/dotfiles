6fe241d3330d1eee66a65af616ab0ae1510dd667
brent <brent@oarengineers.com.au>
2020-08-03 22:00:02 +1000

    initial

---
+++ .emacs.d/elisp/org-outlook.el
@@ -0,0 +1,14 @@
+;;; org-outlook.el - Support for links to Outlook items in Org
+
+(require 'org)
+
+(org-add-link-type "outlook" 'org-outlook-open)
+
+(defun org-outlook-open (id)
+   "Open the Outlook item identified by ID.  ID should be an Outlook GUID."
+   (w32-shell-execute "open" "C:/Program Files/Microsoft Office/root/Office16/OUTLOOK.EXE" (concat "/select " "outlook:" id)
+   ))
+
+(provide 'org-outlook)
+
+;;; org-outlook.el ends here
